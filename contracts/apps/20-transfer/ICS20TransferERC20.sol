54a22f8e8f71734f979de869840b4bd80b2e06b3
Yin Guanhao <sopium@mysterious.site>
2023-09-14 13:43:58 +0800

    Unit test ICS20TransferERC20

--- contracts/apps/20-transfer/ICS20TransferERC20.sol
+++ contracts/apps/20-transfer/ICS20TransferERC20.sol
@@ -6,20 +6,20 @@ import "../../core/25-handler/IBCHandler.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-// An ICS20 implementation that maps denoms to ERC20.
+// An ICS20 implementation that maps denoms to ERC20 contracts.
 contract ICS20TransferERC20 is ICS20Transfer, AccessControlEnumerable {
     // ERC20PresetMinterPauser role. Why can't I reference this?
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    // Map denom to ERC20 contract (ERC20PresetMinterPauser).
-    mapping(string => address) public denomTokenContract;
+    // Map denom to ERC20.
+    mapping(string => ERC20PresetMinterPauser) public denomTokenContract;
 
     constructor(IBCHandler ibcHandler_) ICS20Transfer(ibcHandler_) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function setDenomTokenContract(string calldata denom, address tokenContract) onlyRole(DEFAULT_ADMIN_ROLE) external {
-        require(ERC20PresetMinterPauser(tokenContract).hasRole(MINTER_ROLE, address(this)));
+    function setDenomTokenContract(string calldata denom, ERC20PresetMinterPauser tokenContract) onlyRole(DEFAULT_ADMIN_ROLE) external {
+        require(tokenContract.hasRole(MINTER_ROLE, address(this)));
         denomTokenContract[denom] = tokenContract;
     }
 
@@ -34,7 +34,7 @@ contract ICS20TransferERC20 is ICS20Transfer, AccessControlEnumerable {
     }
 
     function _mint(address account, string memory denom, uint256 amount) internal override returns (bool) {
-        try ERC20PresetMinterPauser(denomTokenContract[denom]).mint(account, amount) {
+        try denomTokenContract[denom].mint(account, amount) {
             return true;
         } catch (bytes memory) {
             return false;
@@ -42,10 +42,24 @@ contract ICS20TransferERC20 is ICS20Transfer, AccessControlEnumerable {
     }
 
     function _burn(address account, string memory denom, uint256 amount) internal override returns (bool) {
-        try ERC20PresetMinterPauser(denomTokenContract[denom]).burnFrom(account, amount) {
+        try denomTokenContract[denom].burnFrom(account, amount) {
             return true;
         } catch (bytes memory) {
             return false;
         }
+    }
+}
+
+// Make external wrappers of _mint/_burn for testing.
+contract ICS20TransferERC20Test is ICS20TransferERC20 {
+    constructor(IBCHandler ibcHandler_) ICS20TransferERC20(ibcHandler_) {
+    }
+
+    function mint(address account, string memory denom, uint256 amount) external returns (bool) {
+        return _mint(account, denom, amount);
+    }
+
+    function burn(address account, string memory denom, uint256 amount) external returns (bool) {
+        return _burn(account, denom, amount);
     }
 }
