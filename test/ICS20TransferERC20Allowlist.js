916cd03578ab12689a434675600981a31c4d3f11
Yin Guanhao <sopium@mysterious.site>
2023-09-15 14:07:29 +0800

    Rename to ICS20TransferERC20Allowlist
    And don't revert on unknown denom

---
+++ test/ICS20TransferERC20Allowlist.js
@@ -0,0 +1,34 @@
+const IBCHandler = artifacts.require("IBCMockHandler");
+const ICS20TransferERC20Allowlist = artifacts.require(
+  "ICS20TransferERC20AllowlistTest"
+);
+const ERC20PresetMinterPauser = artifacts.require("ERC20PresetMinterPauser");
+
+contract("ICS20TransferERC20Allowlist", ([account]) => {
+  it("should be able to mint/burn ERC20", async () => {
+    const ibcHandler = await IBCHandler.deployed();
+    const transfer = await ICS20TransferERC20Allowlist.new(ibcHandler.address);
+    const myToken = await ERC20PresetMinterPauser.new("MyToken", "MT");
+
+    const denom = "/port-2/transfer-8/MY-TOKEN-TYPE-SCRIPT-HASH";
+
+    await myToken.grantRole(await myToken.MINTER_ROLE(), transfer.address);
+    await transfer.setDenomTokenContract(denom, myToken.address);
+
+    await transfer.mintShouldFail(account, "/port/channel/unknown-denom", 100);
+    await transfer.mint(account, denom, 100);
+
+    assert.equal(await myToken.balanceOf(account), 100);
+    assert.equal(await myToken.totalSupply(), 100);
+
+    await transfer.burnShouldFail(account, "/port/channel/unknown-denom", 49);
+    // Burn without allowance should fail.
+    await transfer.burnShouldFail(account, denom, 49);
+
+    await myToken.approve(transfer.address, 49, { from: account });
+    await transfer.burn(account, denom, 49);
+
+    assert.equal(await myToken.balanceOf(account), 51);
+    assert.equal(await myToken.totalSupply(), 51);
+  });
+});
